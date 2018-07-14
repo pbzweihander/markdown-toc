fb106972d0967f3b8b2e17fe475b37fdc5325c73
pbzweihander <sd852456@naver.com>
2018-07-14 19:32:55 +0900

    command line option parsing, command line help message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,213 @@
+extern crate getopts;
+
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+struct Heading {
+    depth: usize,
+    content: String,
+    inner: String,
+}
+
+impl FromStr for Heading {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let trimmed = s.trim();
+        if trimmed.starts_with("#") {
+            let mut depth = 0usize;
+            let content = trimmed
+                .chars()
+                .skip_while(|c| {
+                    if *c == '#' {
+                        depth += 1;
+                        true
+                    } else {
+                        false
+                    }
+                })
+                .collect::<String>()
+                .trim_left()
+                .to_owned();
+            Ok(Heading {
+                depth,
+                content,
+                inner: s.to_owned(),
+            })
+        } else {
+            Err(())
+        }
+    }
+}
+
+enum InputFile {
+    Path(PathBuf),
+    StdIn,
+}
+
+enum Inline {
+    None,
+    Inline,
+    InlineAndReplace,
+}
+
+struct Config {
+    input_file: InputFile,
+    bullet: String,
+    indent: usize,
+    max_depth: usize,
+    min_depth: Option<usize>,
+    header: Option<String>,
+    inline: Inline,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            input_file: InputFile::StdIn,
+            bullet: String::from("1."),
+            indent: 4,
+            max_depth: 0,
+            min_depth: None,
+            header: Some(String::from("## Table of Contents")),
+            inline: Inline::None,
+        }
+    }
+}
+
+impl Config {
+    fn from(opt_matches: &getopts::Matches) -> Result<Self, ()> {
+        let default = Config::default();
+
+        macro_rules! get_opt_or_default {
+            ($o:expr, $p:ident) => {
+                if let Some(val) = opt_matches.opt_get($o).map_err(|_| ())? {
+                    val
+                } else {
+                    default.$p
+                }
+            };
+        }
+        macro_rules! get_opt_or_default_option {
+            ($o:expr, $p:ident) => {
+                opt_matches.opt_get($o).map_err(|_| ())?.or(default.$p)
+            };
+        }
+
+        let (input_file, is_input_stdin) = if !opt_matches.free.is_empty() {
+            if opt_matches.free[0] == "-" {
+                (InputFile::StdIn, true)
+            } else {
+                (InputFile::Path(PathBuf::from(&opt_matches.free[0])), false)
+            }
+        } else {
+            return Err(());
+        };
+
+        Ok(Config {
+            input_file,
+            bullet: get_opt_or_default!("b", bullet),
+            indent: get_opt_or_default!("indent", indent),
+            max_depth: get_opt_or_default!("max-depth", max_depth),
+            min_depth: get_opt_or_default_option!("min-depth", min_depth),
+            header: if opt_matches.opt_present("no-header") {
+                None
+            } else {
+                get_opt_or_default_option!("header", header)
+            },
+            inline: match (
+                opt_matches.opt_present("inline"),
+                opt_matches.opt_present("replace"),
+                is_input_stdin,
+            ) {
+                (true, true, false) => Inline::InlineAndReplace,
+                (true, false, _) => Inline::Inline,
+                (false, false, _) => Inline::None,
+                _ => return Err(()),
+            },
+        })
+    }
+}
+
+fn parse_command(opts: &mut getopts::Options, args: &[String]) -> Result<Config, ()> {
+    opts.optflag("h", "help", "print this help message")
+        .optopt(
+            "b",
+            "bullet",
+            "Custom bullet of the ToC list. (default: \"1.\")",
+            "{str}",
+        )
+        .optopt(
+            "",
+            "indent",
+            "Indentation of the ToC list. (default: 4)",
+            "{int}",
+        )
+        .optopt(
+            "",
+            "max-depth",
+            "Max depth of headers to include. (default: 0)",
+            "{int}",
+        )
+        .optopt("", "min-depth", "Min depth of headers to include.", "{int}")
+        .optopt(
+            "h",
+            "header",
+            "Custom header of the ToC. (default: \"## Table of Contents\")",
+            "{str}",
+        )
+        .optflag("", "no-header", "Exclude the header of ToC")
+        .optflag(
+            "i",
+            "inline",
+            "With this flag, the full markdown file will be printed with ToC."
+        )
+        .optflag(
+            "r",
+            "replace", "Should be used with --inline option and FILE should not be stdin. The original file will be replace instead of printing to standard output."
+        );
+
+    match opts.parse(args) {
+        Ok(m) => {
+            if m.opt_present("h") {
+                Err(())
+            } else {
+                Config::from(&m)
+            }
+        }
+        Err(_) => Err(()),
+    }
+}
+
+fn print_help(program: &str, opts: &getopts::Options) {
+    let brief = format!("Usage: {} FILE [options]\n\n   FILE        The Markdown file to parse for table of contents,\n               or \"-\" to read from stdin", program);
+    eprint!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<_>>();
+    let program = args[0].clone();
+
+    let mut opts = getopts::Options::new();
+    let config = match parse_command(&mut opts, &args[1..]) {
+        Ok(c) => c,
+        Err(_) => {
+            print_help(&program, &opts);
+            return;
+        }
+    };
+
+    let mut content = String::new();
+    match config.input_file {
+        InputFile::StdIn => std::io::stdin().read_to_string(&mut content),
+        InputFile::Path(p) => File::open(p).unwrap().read_to_string(&mut content),
+    }.unwrap();
+
+    content
+        .lines()
+        .map(Heading::from_str)
+        .filter_map(Result::ok)
+        .for_each(|h| println!("{}", h.inner));
 }
