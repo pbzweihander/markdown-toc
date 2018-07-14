e0358c5c24190581fcce9afcb456c029a31f57c0
pbzweihander <sd852456@naver.com>
2018-07-14 20:13:42 +0900

    finished without inlining feature

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ use std::str::FromStr;
 
 struct Heading {
     depth: usize,
-    content: String,
-    inner: String,
+    title: String,
 }
 
 impl FromStr for Heading {
@@ -18,7 +17,7 @@ impl FromStr for Heading {
         let trimmed = s.trim();
         if trimmed.starts_with("#") {
             let mut depth = 0usize;
-            let content = trimmed
+            let title = trimmed
                 .chars()
                 .skip_while(|c| {
                     if *c == '#' {
@@ -32,9 +31,8 @@ impl FromStr for Heading {
                 .trim_left()
                 .to_owned();
             Ok(Heading {
-                depth,
-                content,
-                inner: s.to_owned(),
+                depth: depth - 1,
+                title,
             })
         } else {
             Err(())
@@ -47,20 +45,21 @@ enum InputFile {
     StdIn,
 }
 
-enum Inline {
-    None,
-    Inline,
-    InlineAndReplace,
-}
+// enum Inline {
+//     None,
+//     Inline,
+//     InlineAndReplace,
+// }
 
 struct Config {
     input_file: InputFile,
     bullet: String,
     indent: usize,
-    max_depth: usize,
-    min_depth: Option<usize>,
+    max_depth: Option<usize>,
+    min_depth: usize,
     header: Option<String>,
-    inline: Inline,
+    no_link: bool,
+    // inline: Inline,
 }
 
 impl Default for Config {
@@ -69,10 +68,11 @@ impl Default for Config {
             input_file: InputFile::StdIn,
             bullet: String::from("1."),
             indent: 4,
-            max_depth: 0,
-            min_depth: None,
+            max_depth: None,
+            min_depth: 0,
+            no_link: false,
             header: Some(String::from("## Table of Contents")),
-            inline: Inline::None,
+            // inline: Inline::None,
         }
     }
 }
@@ -96,37 +96,46 @@ impl Config {
             };
         }
 
-        let (input_file, is_input_stdin) = if !opt_matches.free.is_empty() {
-            if opt_matches.free[0] == "-" {
-                (InputFile::StdIn, true)
-            } else {
-                (InputFile::Path(PathBuf::from(&opt_matches.free[0])), false)
-            }
-        } else {
-            return Err(());
-        };
+        // let (input_file, is_input_stdin) = if !opt_matches.free.is_empty() {
+        //     if opt_matches.free[0] == "-" {
+        //         (InputFile::StdIn, true)
+        //     } else {
+        //         (InputFile::Path(PathBuf::from(&opt_matches.free[0])), false)
+        //     }
+        // } else {
+        //     return Err(());
+        // };
 
         Ok(Config {
-            input_file,
-            bullet: get_opt_or_default!("b", bullet),
+            input_file: if !opt_matches.free.is_empty() {
+                if opt_matches.free[0] == "-" {
+                    InputFile::StdIn
+                } else {
+                    InputFile::Path(PathBuf::from(&opt_matches.free[0]))
+                }
+            } else {
+                return Err(());
+            },
+            bullet: get_opt_or_default!("bullet", bullet),
             indent: get_opt_or_default!("indent", indent),
-            max_depth: get_opt_or_default!("max-depth", max_depth),
-            min_depth: get_opt_or_default_option!("min-depth", min_depth),
+            max_depth: get_opt_or_default_option!("max-depth", max_depth),
+            min_depth: get_opt_or_default!("min-depth", min_depth),
+            no_link: opt_matches.opt_present("no-link"),
             header: if opt_matches.opt_present("no-header") {
                 None
             } else {
                 get_opt_or_default_option!("header", header)
             },
-            inline: match (
-                opt_matches.opt_present("inline"),
-                opt_matches.opt_present("replace"),
-                is_input_stdin,
-            ) {
-                (true, true, false) => Inline::InlineAndReplace,
-                (true, false, _) => Inline::Inline,
-                (false, false, _) => Inline::None,
-                _ => return Err(()),
-            },
+            // inline: match (
+            //     opt_matches.opt_present("inline"),
+            //     opt_matches.opt_present("replace"),
+            //     is_input_stdin,
+            // ) {
+            //     (true, true, false) => Inline::InlineAndReplace,
+            //     (true, false, _) => Inline::Inline,
+            //     (false, false, _) => Inline::None,
+            //     _ => return Err(()),
+            // },
         })
     }
 }
@@ -134,7 +143,7 @@ impl Config {
 fn parse_command(opts: &mut getopts::Options, args: &[String]) -> Result<Config, ()> {
     opts.optflag("h", "help", "print this help message")
         .optopt(
-            "b",
+            "",
             "bullet",
             "Custom bullet of the ToC list. (default: \"1.\")",
             "{str}",
@@ -145,29 +154,30 @@ fn parse_command(opts: &mut getopts::Options, args: &[String]) -> Result<Config,
             "Indentation of the ToC list. (default: 4)",
             "{int}",
         )
+        .optopt("", "max-depth", "Max depth of headers to include.", "{int}")
         .optopt(
             "",
-            "max-depth",
-            "Max depth of headers to include. (default: 0)",
+            "min-depth",
+            "Min depth of headers to include. (default: 0)",
             "{int}",
         )
-        .optopt("", "min-depth", "Min depth of headers to include.", "{int}")
         .optopt(
-            "h",
+            "",
             "header",
             "Custom header of the ToC. (default: \"## Table of Contents\")",
             "{str}",
         )
-        .optflag("", "no-header", "Exclude the header of ToC")
-        .optflag(
-            "i",
-            "inline",
-            "With this flag, the full markdown file will be printed with ToC."
-        )
-        .optflag(
-            "r",
-            "replace", "Should be used with --inline option and FILE should not be stdin. The original file will be replace instead of printing to standard output."
-        );
+        .optflag("", "no-link", "Exclude links in ToC")
+        .optflag("", "no-header", "Exclude the header of ToC");
+    // .optflag(
+    //     "i",
+    //     "inline",
+    //     "With this flag, the full markdown file will be printed with ToC."
+    // )
+    // .optflag(
+    //     "",
+    //     "replace", "Should be used with --inline option and FILE should not be stdin. The original file will be replace instead of printing to standard output."
+    // );
 
     match opts.parse(args) {
         Ok(m) => {
@@ -202,12 +212,30 @@ fn main() {
     let mut content = String::new();
     match config.input_file {
         InputFile::StdIn => std::io::stdin().read_to_string(&mut content),
-        InputFile::Path(p) => File::open(p).unwrap().read_to_string(&mut content),
+        InputFile::Path(ref p) => File::open(p).unwrap().read_to_string(&mut content),
     }.unwrap();
+
+    if let Some(ref header) = config.header {
+        println!("{}\n", header);
+    }
 
     content
         .lines()
         .map(Heading::from_str)
         .filter_map(Result::ok)
-        .for_each(|h| println!("{}", h.inner));
+        .for_each(|h| {
+            if h.depth >= config.min_depth && config.max_depth.map(|d| h.depth <= d).unwrap_or(true)
+            {
+                println!(
+                    "{}{} {}",
+                    " ".repeat(config.indent).repeat(h.depth - config.min_depth),
+                    &config.bullet,
+                    if config.no_link {
+                        h.title
+                    } else {
+                        format!("[{}](#{})", h.title, h.title.replace(" ", "-"))
+                    }
+                );
+            }
+        });
 }
